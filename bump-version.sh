f06353a444bda43bab3bcb3f27a196d5d47661e6
thisisthekap <c.kapplmueller@me.com>
2021-06-02 17:28:31 +0200

    fixed nuget restore

--- bump-version.sh
+++ bump-version.sh
@@ -7,6 +7,7 @@ git pull
 sed -i '' "s/<Version>.*<\/Version>/<Version>$1<\/Version>/" Xamarin.ExoPlayerSlim/Xamarin.ExoPlayerSlim.csproj
 sed -i '' "s/<Version>.*<\/Version>/<Version>$1<\/Version>/" Xamarin.ExoPlayerSlim.UsageChecker/Xamarin.ExoPlayerSlim.UsageChecker.csproj
 git add Xamarin.ExoPlayerSlim/Xamarin.ExoPlayerSlim.csproj
+git add Xamarin.ExoPlayerSlim.UsageChecker/Xamarin.ExoPlayerSlim.UsageChecker.csproj
 git commit -m "bumped version to $1"
 git push
 git pull
