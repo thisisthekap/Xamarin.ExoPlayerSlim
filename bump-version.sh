d460808cdb7d18de640600af61b35cb269cdd652
thisisthekap <c.kapplmueller@me.com>
2021-06-02 17:10:29 +0200

    fixed bump-version.sh

--- bump-version.sh
+++ bump-version.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne 1 ]; then
 fi
 git pull
 sed -i '' "s/<Version>.*<\/Version>/<Version>$1<\/Version>/" Xamarin.ExoPlayerSlim/Xamarin.ExoPlayerSlim.csproj
+sed -i '' "s/<Version>.*<\/Version>/<Version>$1<\/Version>/" Xamarin.ExoPlayerSlim.UsageChecker/Xamarin.ExoPlayerSlim.UsageChecker.csproj
 git add Xamarin.ExoPlayerSlim/Xamarin.ExoPlayerSlim.csproj
 git commit -m "bumped version to $1"
 git push
